ce776295b5d562d000a10b12448f10cf218a70cb
Donny Barclay <skibo1122@yahoo.com>
2017-09-30 22:53:24 -0700

    switched strings and ints to Unreal preferred syntax of FString, FText, and int32 across the project.

--- BullCowGame/FBullCowBull.h
+++ BullCowGame/FBullCowBull.h
@@ -1,19 +1,22 @@
 #pragma once
 #include <string>
 
+using FString = std::string;
+using int32 = int;
+
 class FBullCowGame {
 public:
 	FBullCowGame(); //constructor
 
-	int GetMaxTries() const;
-	int GetCurrentTry() const;
+	int32 GetMaxTries() const;
+	int32 GetCurrentTry() const;
 	bool IsGameWon() const;
 
 	void Reset(); //TODO make a more rich return value
-	bool CheckGuessValidity(std::string); //TODO make a more rich return value
+	bool CheckGuessValidity(FString); //TODO make a more rich return value
 	// provide a method for counting bulls and cows and incrementing turn number
 
 private:
-	int MyCurrentTry = 1;
-	int MyMaxTries = 5;
+	int32 MyCurrentTry = 1;
+	int32 MyMaxTries = 5;
 };
--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,10 +1,19 @@
+/* This is the console executable that makes use of the BullCowClass
+This acts as the view in a MVC pattern, and is resposible for all
+user interaction.  For game logic see the FBullCowGame
+
+*/
+
 #include <iostream>
 #include <string>
 #include "FBullCowBull.h"
 
+using FText = std::string;
+using int32 = int;
+
 void PrintIntro();
 void PlayGame();
-std::string GetGuess();
+FText GetGuess();
 bool AskToPlayAgain();
 FBullCowGame BCGame;
 
@@ -25,7 +34,7 @@ int main() {
 // introduce the game
 void PrintIntro() 
 {	
-	constexpr int WORD_LENGTH = 5;
+	constexpr int32 WORD_LENGTH = 5;
 	std::cout << "Welcome to Bulls and Cows, a fun word game." << std::endl;
 	std::cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of\n";
 	std::cout << std::endl;
@@ -35,13 +44,13 @@ void PrintIntro()
 void PlayGame()
 {
 	BCGame.Reset();
-	int MaxTries = BCGame.GetMaxTries();  
+	int32 MaxTries = BCGame.GetMaxTries();  
 
 	// loop for number of turns asking for guesses
 	// TODO change from FOR to WHILE once we are validating tries
-	for (int count = 0; count < MaxTries; count++)
+	for (int32 count = 0; count < MaxTries; count++)
 	{
-		std::string Guess = GetGuess();  // TODO make loop check for valid answers
+		FText Guess = GetGuess();  // TODO make loop check for valid answers
 		// submit valid guess to game
 		// print number of bulls and cows
 		std::cout << "Your guess was: " << Guess << std::endl;
@@ -51,15 +60,14 @@ void PlayGame()
 	// TODO add a game summary
 }
 
-std::string GetGuess() 
+FText GetGuess() 
 {
 	
-	int CurrentTry = BCGame.GetCurrentTry();
-	std::cout << CurrentTry << std::endl;
+	int32 CurrentTry = BCGame.GetCurrentTry();
 
 	// get a guess from the player
 	std::cout << "Enter your guess: ";
-	std::string Guess = "";
+	FText Guess = "";
 	std::getline(std::cin, Guess);
 	return Guess;
 }
@@ -67,7 +75,7 @@ std::string GetGuess()
 bool AskToPlayAgain()
 {
 	std::cout << "Do you want to play again? (y/n)";
-	std::string Response = "";
+	FText Response = "";
 	std::getline(std::cin, Response);
 
 	return (Response[0] == 'y') || (Response[0] == 'Y');
