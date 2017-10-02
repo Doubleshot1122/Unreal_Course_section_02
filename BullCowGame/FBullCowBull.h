3a0494fc67228c7c36fca5a9e94299b5ccfb9597
Donny Barclay <skibo1122@yahoo.com>
2017-10-01 19:44:41 -0700

    created enum class EWordStatus

--- BullCowGame/FBullCowBull.h
+++ BullCowGame/FBullCowBull.h
@@ -10,10 +10,12 @@ struct FBullCowCount
 	int32 Cows = 0;
 };
 
-enum EWordStatus 
+enum class EWordStatus 
 {
 	OK,
-	Not_Isogram
+	Not_Isogram,
+	Wrong_Length,
+	Not_Lowercase
 };
 
 class FBullCowGame {
